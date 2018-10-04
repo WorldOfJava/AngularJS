dae32d06dc017258c4f45ff2dc4e5c483a4130e5
WorldOfJava <43439296+WorldOfJava@users.noreply.github.com>
2018-10-04 15:53:19 +0200

    Add files via upload

---
+++ Angular-Tour-of-Heroes/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Son Goku' },
+  { id: 2, name: 'Saitama' },
+  { id: 3, name: 'Deku' },
+  { id: 4, name: 'Bakugo' },
+  { id: 5, name: 'Uraraka' },
+  { id: 6, name: 'All Might' },
+  { id: 7, name: 'Joker' },
+  { id: 8, name: 'Ann Takamaki' },
+  { id: 9, name: 'Spiderman' },
+  { id: 10, name: 'ColeInfamousdark' },
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
